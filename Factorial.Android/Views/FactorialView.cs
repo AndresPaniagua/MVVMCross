9f3e6f6e770bdabfeca07cd87552b4e4eb34828d
ANDRES FELIPE PANIAGUA LEMA <andrespaniagua250958@correo.itm.edu.co>
2020-05-20 13:13:55 -0500

    Finish factorial Droid

---
+++ Factorial.Android/Views/FactorialView.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Factorial.Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Factorial.Android.Views
+{
+    [Activity(Label = "@string/app_name")]
+    internal class FactorialView : MvxActivity<FactorialViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.FactorialPage);
+        }
+    }
+
+}
